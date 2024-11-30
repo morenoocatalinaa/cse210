96d8596512fffe8223bc2d449614172d8c305b6c
morenoocatalinaa <morenoocatalina@gmail.com>
2024-11-30 20:35:18 -0300

    Agregados los archivos de Video, Comment y Program

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -1,9 +1,55 @@
 using System;
 
-class Program
+public class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video video1 = new Video
+        {
+            Title = "C# Tutorial",
+            Author = "John Doe",
+            Length = 15
+        };
+        video1.AddComment(new Comment("Alice", "Great video!"));
+        video1.AddComment(new Comment("Bob", "I learned a lot."));
+
+        Video video2 = new Video
+        {
+            Title = "Learn LINQ",
+            Author = "Jane Smith",
+            Length = 20
+        };
+        video2.AddComment(new Comment("Charlie", "Very informative!"));
+
+        Video video3 = new Video
+        {
+            Title = "Mastering Arrays",
+            Author = "Bob Johnson",
+            Length = 25
+        };
+        video3.AddComment(new Comment("David", "Clear explanation!"));
+
+        Console.WriteLine($"Title: {video1.Title}, Author: {video1.Author}, Length: {video1.Length} mins");
+        Console.WriteLine($"Number of Comments: {video1.GetCommentCount()}");
+        foreach (var comment in video1.GetComments())
+        {
+            Console.WriteLine($"{comment.Name}: {comment.Text}");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Title: {video2.Title}, Author: {video2.Author}, Length: {video2.Length} mins");
+        Console.WriteLine($"Number of Comments: {video2.GetCommentCount()}");
+        foreach (var comment in video2.GetComments())
+        {
+            Console.WriteLine($"{comment.Name}: {comment.Text}");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine($"Title: {video3.Title}, Author: {video3.Author}, Length: {video3.Length} mins");
+        Console.WriteLine($"Number of Comments: {video3.GetCommentCount()}");
+        foreach (var comment in video3.GetComments())
+        {
+            Console.WriteLine($"{comment.Name}: {comment.Text}");
+        }
     }
 }
---
+++ foundation/Foundation1/Video.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public int Length { get; set; }
+    private List<Comment> comments = new List<Comment>();
+
+    public void AddComment(Comment comment)
+    {
+        comments.Add(comment);
+    }
+
+    public int GetCommentCount()
+    {
+        return comments.Count;
+    }
+
+    public List<Comment> GetComments()
+    {
+        return comments;
+    }
+}
+
